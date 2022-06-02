d6fd41a5bdbdc5c8acb71f7351e7568b33742c3d
MiminZku <82875083+MiminZku@users.noreply.github.com>
2022-06-03 06:53:28 +0900

    주문내역

--- baedal_moa_v1/lib/Pages/OrderLogPage.dart
+++ baedal_moa_v1/lib/Pages/OrderLogPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../Model/Menu.dart';
 import '../Model/Order.dart';
@@ -15,95 +16,115 @@ class OrderLogPage extends StatefulWidget {
 }
 
 class _OrderLogPageState extends State<OrderLogPage> {
-  late List<Order> orderList;
+  late List<Order> orderList = [];
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     Services_Order.getOrders(widget.userId.toString()).then((value) {
-      orderList = value;
+      setState(() {
+        orderList = value;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            child: Text('내 주문 내역', style: TextStyle(fontSize: 25)),
-            padding: EdgeInsets.only(left: 10, top: 5),
-          ),
-          getOrderList(),
-          line,
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '롯데리아 경기대역점',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '주문 번호 : ' + '',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('새우버거 세트' + ' x ' + '1',
-                        style: TextStyle(color: Colors.grey)),
-                    Text('개수 X 가격', style: TextStyle(color: Colors.grey))
-                  ],
-                ),
-                Text('총 결제 포인트 : ' + '14000원',
-                    style: TextStyle(color: Colors.black)),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+    return orderList.isEmpty
+        ? Center(
+            child: Text('주문 내역이 없습니다.'),
+          )
+        : SingleChildScrollView(
+            child: getOrderList(),
+          );
   }
 
-  @override
-  Widget line = Container(
-      padding: const EdgeInsets.only(left: 10, right: 10),
-      child: Divider(color: Colors.black, thickness: 1.0));
-
   getOrderList() {
-    Map<int,List<OrderedMenu>> orderMenuList = {};
-    for(Order o in orderList){
-      if(orderMenuList.containsKey(o.roomId)){
-        orderMenuList[o.roomId]?.add(OrderedMenu(menuPrice: o.menuPrice, menuName: o.menuName, menuCount: o.menuCount));
-      }else{
-        orderMenuList.
+    List<int> orderNumList = [];
+    Map<int, OrderLog> orderLogList = {};
+    for (Order o in orderList) {
+      if (orderLogList.containsKey(o.roomId)) {
+        orderLogList[o.roomId]?.menuList.add(OrderedMenu(
+            menuPrice: o.menuPrice,
+            menuName: o.menuName,
+            menuCount: o.menuCount));
+        orderLogList[o.roomId]!.totalPrice =
+            orderLogList[o.roomId]!.totalPrice + o.menuPrice * o.menuCount;
+      } else {
+        orderNumList.add(o.roomId);
+        orderLogList.addAll({
+          o.roomId: OrderLog(
+              orderId: o.roomId,
+              orderedTime: o.roomExpireTime,
+              resName: o.resName,
+              menuList: [
+                OrderedMenu(
+                    menuPrice: o.menuPrice,
+                    menuName: o.menuName,
+                    menuCount: o.menuCount)
+              ],
+              totalPrice: o.menuCount * o.menuPrice + o.roomDelFee,
+              orderDelfee: o.roomDelFee)
+        });
       }
     }
-    return Column(
-      children: [
-        for(Order o in orderList)
-          Column(
-            children: [
-              Text(
-                //o.resName,
-                '롯데리아 경기대역점',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return Column(children: [
+      for (int i in orderNumList.reversed)
+        Column(
+          children: [
+            Text(
+              //o.resName,
+              '롯데리아 경기대역점',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text('주문 번호 : ' + orderLogList[i]!.orderId.toString()),
+            for (OrderedMenu om in orderLogList[i]!.menuList)
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(om.menuName + ' x ' + om.menuCount.toString()),
+                    Text((om.menuCount * om.menuPrice).toString() + ' 원')
+                  ],
+                ),
               ),
-              Text('주문 번호 : ' + o.),
-            ],
-          )]);
+            Text('배달비 : ' + orderLogList[i]!.orderDelfee.toString() + ' 원'),
+            Text('총 결제 금액 : ' + orderLogList[i]!.totalPrice.toString() + ' 원'),
+            Text('주문 완료 시간 : ' +
+                DateFormat('yyyy-MM-dd hh:mm')
+                    .format(orderLogList[i]!.orderedTime)),
+            Container(
+              height: 1,
+              color: Colors.deepOrange,
+            ),
+          ],
+        )
+    ]);
   }
 }
 
-class OrderedMenu{
+class OrderLog {
+  OrderLog(
+      {required this.orderId,
+      required this.resName,
+      required this.orderedTime,
+      required this.menuList,
+      required this.totalPrice,
+      required this.orderDelfee});
+  int orderId;
+  String resName;
+  DateTime orderedTime;
+  List<OrderedMenu> menuList;
+  int totalPrice;
+  int orderDelfee;
+}
+
+class OrderedMenu {
   OrderedMenu({
     required this.menuPrice,
     required this.menuName,
     required this.menuCount,
-
   });
   String menuName;
   int menuPrice;
